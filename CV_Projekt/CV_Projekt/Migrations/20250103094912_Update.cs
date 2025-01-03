2c4e30176a9a71e1bf2a634cb6bc944d408ea8db
RebeckaLewin <rebeckalewin@hotmail.com>
2025-01-03 10:49:46 +0100

    La till dbset messages

---
+++ CV_Projekt/CV_Projekt/Migrations/20250103094912_Update.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CV_Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Users_RecieverId",
+                table: "Message");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Users_SenderId",
+                table: "Message");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Message",
+                table: "Message");
+
+            migrationBuilder.RenameTable(
+                name: "Message",
+                newName: "Messages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_SenderId",
+                table: "Messages",
+                newName: "IX_Messages_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_RecieverId",
+                table: "Messages",
+                newName: "IX_Messages_RecieverId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages",
+                column: "Id");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "61ede7f6-e023-4a30-9730-22c3d5b0da71", "ed1f10fb-e337-4ddb-9c35-b621e696a75a" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "94c68715-4b1e-44b0-9666-2ed205b6cdfb", "729a8f52-75e3-4954-b2e5-612e73f15543" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "df6befc0-c0a9-4007-ad82-d8cebb476826", "5e52a9a9-7cf2-41f8-8627-9de69c9b8d40" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_RecieverId",
+                table: "Messages",
+                column: "RecieverId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_RecieverId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages");
+
+            migrationBuilder.RenameTable(
+                name: "Messages",
+                newName: "Message");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_SenderId",
+                table: "Message",
+                newName: "IX_Message_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_RecieverId",
+                table: "Message",
+                newName: "IX_Message_RecieverId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Message",
+                table: "Message",
+                column: "Id");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "6ead067d-d776-4e19-a5d9-6ab721cef856", "71883185-2a3a-4e87-9b46-dfe6965e6041" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "654f41cf-7278-4e18-b235-b4c9bd2942a0", "207064f6-b515-4163-b294-290566c1a627" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "9f4a0a84-613c-43a8-8782-1ffa70317778", "a15d4eca-4113-4a31-a375-315ccda89e3c" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Users_RecieverId",
+                table: "Message",
+                column: "RecieverId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Users_SenderId",
+                table: "Message",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
