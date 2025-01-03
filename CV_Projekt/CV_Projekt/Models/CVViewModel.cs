d1d5bf15c6f134d1036496574fe838ab01101b8d
knemil <153902555+knemil@users.noreply.github.com>
2025-01-03 14:33:31 +0100

    wtf

--- CV_Projekt/CV_Projekt/Models/CVViewModel.cs
+++ CV_Projekt/CV_Projekt/Models/CVViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class CVViewModel
     {
+        public List<CV> Cvs { get; set; }
+        public List<User> Users { get; set; }
+
+        public List<Project> Projects { get; set; }
+
+        public List<Experience> Experiences { get; set; }
+
+        public List<Tag> Tags { get; set; }
+        public List<string> skills {  get; set; } 
     }
 }
