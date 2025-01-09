5d7da160f458bfb7f83f3432565340010b9f26b6
JillWithJ <linneawilcoxx@gmail.com>
2025-01-09 14:59:24 +0100

    tjoooo

---
+++ CV_Projekt/CV_Projekt/Controllers/AnnanMessController.cs
@@ -0,0 +1,41 @@
+﻿using CV_Projekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CV_Projekt.Controllers
+{
+	public class AnnanMessController : Controller
+	{
+		private CvContext _context;
+
+		public AnnanMessController(CvContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult AllUserList()
+		{
+			var allUsers = _context.Users.ToList();
+
+            var viewModel = new UserViewModel
+            {
+                _users = allUsers
+            };
+            return View();
+		}
+
+		[HttpGet]
+		public IActionResult NewAdd()
+		{
+
+		}
+
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
