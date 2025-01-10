8eb8c665e3302f26c337f39d692d9b3f84f99108
RebeckaLewin <rebeckalewin@hotmail.com>
2025-01-10 13:21:34 +0100

    Fixade profil

--- CV_Projekt/CV_Projekt/Models/UserProfileViewModel.cs
+++ CV_Projekt/CV_Projekt/Models/UserProfileViewModel.cs
@@ -2,17 +2,10 @@
 {
     public class UserProfileViewModel
     {
-        public List<CV> Cvs { get; set; }
         public User User { get; set; }
+        public List<Project> Projects { get; set; }
 
-        public ContactInformation ContactInformation { get; set; }
-
-        public List<Project> ProjectsCreated { get; set; }
-        public List<Project> ProjectCollaborator { get; set; }
-
-        public List<Experience> Experiences { get; set; }
-
-        public List<string> Skills { get; set; }
+        //public List<string> Skills { get; set; }
         public List<Tag> Tags { get; set; }
     }
 }
