5652f1aad2646729c3d6360cc47fed18bcc017e1
knemil <153902555+knemil@users.noreply.github.com>
2025-01-10 10:28:15 +0100

    projekt-länk från profil

--- CV_Projekt/CV_Projekt/Controllers/ProfileProjectController.cs
+++ CV_Projekt/CV_Projekt/Controllers/ProfileProjectController.cs
@@ -12,11 +12,30 @@ namespace CV_Projekt.Controllers
             _context = context;
         }
         [HttpGet]
-        public IActionResult ProfileProject()
+        public IActionResult ProfileProject(string id)
         {
+            var user = _context.Users
+                .Where(u => u.Id == id)
+                .FirstOrDefault();
+            if (user == null) 
+            {
+                return NotFound();
+            }
+
+            var projPart = _context.Projects
+                .Where(p => p.Participants.
+                Any(u => u.Id == id))
+                .ToList();
+
+            var projCreated = _context.Projects
+                .Where(p => p.CreatorId == id)
+                .ToList();
+
             ProfileProjectViewModel ppvm = new ProfileProjectViewModel
             {
-
+                User = user,
+                ProjectParticipant = projPart,
+                CreatedProjects = projCreated
             };
             return View(ppvm);
         }
