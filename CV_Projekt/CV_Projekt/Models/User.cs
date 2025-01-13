85e91937e92300587c1ff2a38581214e65b96b20
RebeckaLewin <rebeckalewin@hotmail.com>
2025-01-13 09:59:20 +0100

    kajs

--- CV_Projekt/CV_Projekt/Models/User.cs
+++ CV_Projekt/CV_Projekt/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
