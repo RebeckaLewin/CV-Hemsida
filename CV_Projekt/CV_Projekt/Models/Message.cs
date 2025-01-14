44df932f6f318bf8125bdd41e65a41caf1b13491
RebeckaLewin <rebeckalewin@hotmail.com>
2025-01-14 16:36:18 +0100

    Revert "Update Message.cs"
    This reverts commit 4ab0c467c23858f2b8818aade5cff900581ef0a7.

--- CV_Projekt/CV_Projekt/Models/Message.cs
+++ CV_Projekt/CV_Projekt/Models/Message.cs
@@ -6,36 +6,26 @@ namespace CV_Projekt.Models
 	public class Message
 	{
 		public int Id { get; set; }
-		[Required(ErrorMessage ="Ämne måste anges.")]
+		[Required(ErrorMessage ="Ämne måste anges")]
 		public string Subject { get; set; }
-		[Required(ErrorMessage ="Meddelandet måste ha ett innehåll.")]
+		[Required(ErrorMessage ="Meddelandet måste ha ett innehåll")]
         [MaxLength(300, ErrorMessage = "Innehållet får inte överstiga 300 tecken")]
 		public string Content { get; set; }
-		[Required(ErrorMessage = "Meddelandet måste ha ett datum.")]
+		[Required]
 		public DateTime Date { get; set; }
+		[Required]
 		public string? SenderId { get; set; }
 		[Required]
-		public string SenderName { get; set; }
-		[Required(ErrorMessage = "Meddelandet måste ha en mottagare.")]
 		public string? ReceiverId { get; set; }
-		[Required(ErrorMessage = "Meddelandet måste vara läst eller inte.")]
+		[Required]
 		public bool isRead { get; set; }
-
 		[ForeignKey(nameof(SenderId))]
 		public virtual User? Sender { get; set; }
 		[ForeignKey(nameof(ReceiverId))]
 		public virtual User? Receiver { get; set; }
 
-		[Required(ErrorMessage = "Meddelandet måste vara raderat eller inte.")]
 		public bool ReceiverDelete { get; set; }
-		[Required(ErrorMessage = "Meddelandet måste vara raderat eller inte.")]
 		public bool SenderDelete { get; set; }
 
-		public Message()
-		{
-			ReceiverDelete = false;
-			SenderDelete = false;
-			isRead = false;
-		}
 	}
 }
